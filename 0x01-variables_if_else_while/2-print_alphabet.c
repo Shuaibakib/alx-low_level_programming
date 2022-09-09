4355188da684b55b13898977b7720315d71af80b
Akibu <Shuaibakibu@gmail.com>
2022-09-09 09:07:40 -0700

    print alphabets

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -2,7 +2,7 @@
 #include <stdlib.h>
 #include <time.h>
  /**
-  * main - print alphabets in lowercase
+  * main - entry point
   *
   * Return: Always 0 (success)
   */
@@ -10,7 +10,6 @@
 int main(void)
 {
 	char low;
-	
 	for  (low = 'a'; low <= 'z'; low++)
 		putchar(low);
 	putchar('\n');
