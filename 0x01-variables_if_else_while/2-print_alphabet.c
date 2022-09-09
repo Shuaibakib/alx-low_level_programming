65a6a243a44861bdb9d0c4a1565debcfa6f6e686
Akibu <Shuaibakibu@gmail.com>
2022-09-09 09:02:43 -0700

    print alphabets

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -4,12 +4,13 @@
  /**
   * main - print alphabets in lowercase
   *
-  * Return; Always 0 (success)
+  * Return: Always 0 (success)
   */
 
 int main(void)
 {
 	char low;
+	
 	for  (low = 'a'; low <= 'z'; low++)
 		putchar(low);
 	putchar('\n');
